bb03714e84f44d9e738af84251053011263e01c5
Mateusz Szczech <matle0410@gmail.com>
2024-08-25 11:17:50 +0200

    Cache first

--- client/build/sw.js
+++ client/build/sw.js
@@ -60,18 +60,20 @@ self.addEventListener("fetch", function (event) {
   if (request.method === "GET") {
     if (cachedTypes.includes(request.destination)) {
       event.respondWith(caches.open(cacheName).then(function (cache) {
-        return fetch(event.request.url).then(function (fetchedResponse) {
-          cache.put(event.request, fetchedResponse.clone());
-          return fetchedResponse;
-        })["catch"](function () {
-          return cache.match(event.request.url);
+        return cache.match(event.request.url).then(function (cachedResponse) {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+          return fetch(event.request).then(function (fetchedResponse) {
+            cache.put(event.request, fetchedResponse.clone());
+            return fetchedResponse;
+          });
         });
       }));
     }
   }
   return;
 });
-
 /*
 
 ### Network first, falling back to cache ###
